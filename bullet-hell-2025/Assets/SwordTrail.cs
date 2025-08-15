6f944dd4d787f0802dccf7b84978d7dc432a2f5d
Skyglader <andyhoangnguyen2005@gmail.com>
2025-08-15 01:29:17 -0700

    setup ik for rifle enemy

--- bullet-hell-2025/Assets/SwordTrail.cs
+++ bullet-hell-2025/Assets/SwordTrail.cs
@@ -92,27 +92,87 @@ public class SwordTrail : MonoBehaviour
         frameCount += 12;
 
 
-        for(int i = 0; i < frameCount + 12; i += 6)
-{
-            // Calculate the segment index along the trail
-            float segment = i / 6f; // each 6 vertices = 1 segment
-            float v0 = segment / trailFrameLength;       // start of segment along trail
-            float v1 = (segment + 1) / trailFrameLength; // end of segment along trail
+        mesh.uv = UvCalculator.CalculateUVs(vertices, 1);
+        mesh.RecalculateTangents();
 
-            // Bottom-left, top-left, top-right for first triangle
-            uvs[i] = new Vector2(0, v0); // base-left
-            uvs[i + 1] = new Vector2(1, v0); // base-right
-            uvs[i + 2] = new Vector2(1, v1); // tip-right
+    }
 
-            // Second triangle
-            uvs[i + 3] = new Vector2(0, v0); // base-left duplicate
-            uvs[i + 4] = new Vector2(1, v1); // tip-right duplicate
-            uvs[i + 5] = new Vector2(0, v1); // tip-left
+    public class UvCalculator
+    {
+        private enum Facing { Up, Forward, Right };
+
+        public static Vector2[] CalculateUVs(Vector3[] v/*vertices*/, float scale)
+        {
+            var uvs = new Vector2[v.Length];
+
+            for (int i = 0; i < uvs.Length; i += 3)
+            {
+                int i0 = i;
+                int i1 = i + 1;
+                int i2 = i + 2;
+
+                Vector3 v0 = v[i0];
+                Vector3 v1 = v[i1];
+                Vector3 v2 = v[i2];
+
+                Vector3 side1 = v1 - v0;
+                Vector3 side2 = v2 - v0;
+                var direction = Vector3.Cross(side1, side2);
+                var facing = FacingDirection(direction);
+                switch (facing)
+                {
+                    case Facing.Forward:
+                        uvs[i0] = ScaledUV(v0.x, v0.y, scale);
+                        uvs[i1] = ScaledUV(v1.x, v1.y, scale);
+                        uvs[i2] = ScaledUV(v2.x, v2.y, scale);
+                        break;
+                    case Facing.Up:
+                        uvs[i0] = ScaledUV(v0.x, v0.z, scale);
+                        uvs[i1] = ScaledUV(v1.x, v1.z, scale);
+                        uvs[i2] = ScaledUV(v2.x, v2.z, scale);
+                        break;
+                    case Facing.Right:
+                        uvs[i0] = ScaledUV(v0.y, v0.z, scale);
+                        uvs[i1] = ScaledUV(v1.y, v1.z, scale);
+                        uvs[i2] = ScaledUV(v2.y, v2.z, scale);
+                        break;
+                }
+            }
+            return uvs;
         }
 
+        private static bool FacesThisWay(Vector3 v, Vector3 dir, Facing p, ref float maxDot, ref Facing ret)
+        {
+            float t = Vector3.Dot(v, dir);
+            if (t > maxDot)
+            {
+                ret = p;
+                maxDot = t;
+                return true;
+            }
+            return false;
+        }
 
-        mesh.uv = uvs;
-        mesh.RecalculateBounds();
+        private static Facing FacingDirection(Vector3 v)
+        {
+            var ret = Facing.Up;
+            float maxDot = Mathf.NegativeInfinity;
 
+            if (!FacesThisWay(v, Vector3.right, Facing.Right, ref maxDot, ref ret))
+                FacesThisWay(v, Vector3.left, Facing.Right, ref maxDot, ref ret);
+
+            if (!FacesThisWay(v, Vector3.forward, Facing.Forward, ref maxDot, ref ret))
+                FacesThisWay(v, Vector3.back, Facing.Forward, ref maxDot, ref ret);
+
+            if (!FacesThisWay(v, Vector3.up, Facing.Up, ref maxDot, ref ret))
+                FacesThisWay(v, Vector3.down, Facing.Up, ref maxDot, ref ret);
+
+            return ret;
+        }
+
+        private static Vector2 ScaledUV(float uv1, float uv2, float scale)
+        {
+            return new Vector2(uv1 / scale, uv2 / scale);
+        }
     }
 }
